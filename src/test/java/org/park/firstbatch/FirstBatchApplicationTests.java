5e563d53efddbf5e0fe99264f15b94931e0dda07
phj <1004mapa2@naver.com>
2024-04-20 18:40:31 +0900

    setting

---
+++ src/test/java/org/park/firstbatch/FirstBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.park.firstbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
